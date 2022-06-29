aef7357a6e7503c9aa35f54fa3f74363d74a0d45
Chris <miller.christopher618@gmail.com>
2022-06-29 09:17:17 -0500

    Basic high score checker

---
+++ C#/C-Sharp Practice 2/HighScore/HighScore/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace HighScore
+{
+    class Program
+    {
+        static int highscore = 0;
+        static string highplayer = "";
+        static void Main(string[] args)
+        {
+            int score;
+            do
+            {
+                Console.WriteLine("Enter the player");
+                string player = Console.ReadLine();
+                Console.WriteLine("Enter the score (enter negative number to exit");
+                score = Int32.Parse(Console.ReadLine());
+                if(score != -1)
+                {
+                    HighScore(score, player);
+                }
+
+            } while (score < 0);
+        }
+
+        static void HighScore(int score, string player)
+        {
+            if (score > highscore)
+            {
+                highscore = score;
+                highplayer = player;
+                Console.WriteLine("Player " + highplayer + " has taken the top spot with a score of " + highscore);
+            }
+            else
+            {
+                Console.WriteLine("Player " + player + "'s " + score + " did not overtake Player " + highplayer + "'s " + score);
+                Console.WriteLine("Player " + highplayer + " still has the highscore");
+            }
+        }
+    }
+}
